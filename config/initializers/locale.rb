7ad37a0a27b67975cbab4fa774725388797cd50f
meherremoff <m.samxal@box.az>
2016-09-07 14:55:48 +0400

    Updates 14:55

--- config/initializers/locale.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/locale.rb
- 
-# где библиотека I18n должна искать наши переводы
-I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
- 
-# устанавливаем локаль по умолчанию на что-либо другое, чем :en
-I18n.default_locale = :az
